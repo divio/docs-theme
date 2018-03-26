57d21bf60b1ee3d9542bfc97a2aebc1f5d1d1b50
Vadim Sikora <vadim.sikora@divio.ch>
2018-03-26 15:41:52 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,5 @@
+****************
+Divio Docs Theme
+****************
+
+Based on `Read the Docs theme <https://github.com/rtfd/sphinx_rtd_theme>`_
---
+++ setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from io import open
+from setuptools import setup
+from sphinx_rtd_theme import __version__
+
+
+setup(
+    name='divio_docs_theme',
+    version=__version__,
+    url='https://github.com/divio/divio-docs-theme/',
+    license='MIT',
+    author='Divio AG',
+    author_email='developers@divio.com',
+    description='Divio theme for Sphinx',
+    long_description=open('README.rst', encoding='utf-8').read(),
+    zip_safe=False,
+    packages=['divio_docs_theme'],
+    package_data={'divio_docs_theme': [
+        'theme.conf',
+        '*.html',
+        'static/css/*.css',
+        'static/js/*.js',
+        'static/font/*.*'
+    ]},
+    include_package_data=True,
+    entry_points={
+        'sphinx.html_themes': [
+            'divio_docs_theme = divio_docs_theme',
+        ]
+    },
+    classifiers=[
+        'Framework :: Sphinx',
+        'Framework :: Sphinx :: Theme',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+    ],
+)
